1855eaedbec0d0b1934e9cb5d7222bb0da2fed27
a.molchanov <a.molchanov@corp.mail.ru>
2013-01-25 16:06:52 +0400

    Small bug fixed

--- src/main/resources/media/aduserpicker.js
+++ src/main/resources/media/aduserpicker.js
@@ -205,7 +205,7 @@ function addGroup(dataInputId, listDivId, groupsSelectId) {
 
     var newElem = jQuery("<div id='" + grId + "'><span>" + AJS.format(AJS.I18n.getText("jrole-group-usercf.share_group"), jQuery(group).text()) + "</span></div>");
     jQuery("#" + listDivId).append(newElem);
-    jQuery("#" + listDivId + " #share_trash_sh").clone().show().appendTo(newElem);
+    jQuery("#" + listDivId + " #share_trash_sh").clone().attr('id', '').show().appendTo(newElem);
 }
 
 function addProject(dataInputId, listDivId, projectsSelectId, rolesSelectId) {
@@ -239,7 +239,7 @@ function addProject(dataInputId, listDivId, projectsSelectId, rolesSelectId) {
 
     var newElem = jQuery("<div id='" + prId + "'><span>" + textVal + "</span></div>");
     jQuery("#" + listDivId).append(newElem);
-    jQuery("#" + listDivId + " #share_trash_sh").clone().show().appendTo(newElem);
+    jQuery("#" + listDivId + " #share_trash_sh").clone().attr('id', '').show().appendTo(newElem);
 }
 
 function removeGroup(event, dataInputId) {
